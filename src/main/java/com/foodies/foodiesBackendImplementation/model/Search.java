8f7050f0dc90bbba0ebf901c9e6896a3d1087982
vidhushan <vidhushan1017@gmail.com>
2025-01-28 16:38:41 +0530

    search getter and setter

--- src/main/java/com/foodies/foodiesBackendImplementation/model/Search.java
+++ src/main/java/com/foodies/foodiesBackendImplementation/model/Search.java
@@ -22,9 +22,84 @@ public class Search {
         this.query = query;
         this.category =category;
         this.userId = userId;
-        this.timestamp = System.currentTimeMillis()
+        this.timestamp = System.currentTimeMillis();
 
     }
+     // Getters and Setters
+     public String getId(){
+        return id;
+     }
+
+      public void setId(String id){
+        this.id = id;
+      }
+
+      public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Double getPriceRange() {
+        return priceRange;
+    }
+
+    public void setPriceRange(Double priceRange) {
+        this.priceRange = priceRange;
+    }
+
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    public Boolean getIsVegetarian() {
+        return isVegetarian;
+    }
+
+    public void setIsVegetarian(Boolean isVegetarian) {
+        this.isVegetarian = isVegetarian;
+    }
+}
+
+
 
 
 
